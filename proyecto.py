26342ff26c569f0a8680e0f5e600349ef7b1aae8
JonasPorro <62890616+JonasPorro@users.noreply.github.com>
2026-09-15 14:54:43 -0300

    Agrego avances del trabajo

--- proyecto.py
+++ proyecto.py
@@ -1,3 +1,11 @@
-#with open('archivo.txt')
+with open ("estado_tiempo20260910.txt", "r", encoding="cp1252") as archivo:
 
-print(123)
+    datos = archivo.read()
+    datos_limpios = datos.strip().split(";")
+
+    datitos = {"ciudades/estación": datos_limpios[0], 
+               "fecha": datos_limpios[1],
+                "Hora": datos_limpios[2],
+                "condicion_del_cielo" : datos_limpios[3]
+    }
+    print(datitos)
