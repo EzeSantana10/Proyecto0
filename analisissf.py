a87a0ca678b663acaa19e3da2110eb134e2ab035
Ezequiel Santana <ezequielbarron92@outlook.es>
2026-09-15 21:52:50 -0300

    Agrego datos parceados

--- analisissf.py
+++ analisissf.py
@@ -1,6 +1,40 @@
+dato_limpio = []
+diccionario_datos = {
+    "Ciudad/Estación": [],
+    "Fecha": [],
+    "Hora": [],
+    "Condición del cielo": [],
+    "Visibilidad": [],
+    "Temperatura": [],
+    "Sensación térmica": [],
+    "Humedad": [],
+    "Viento": [],
+    "Presión": []
+}
+
 with open ("estado_tiempo20260910.txt", "r", encoding="cp1252") as archivo:
 
-    datos = archivo.read()
-    datos_limpios = datos.strip().split(";")
+    for linea in archivo:
+        linea1 = linea.strip("\n")
+        columnas = linea1.split(";")
+        dato_limpio.append(columnas)
 
-   print(datos_limpios)
+    for datos in dato_limpio:
+        diccionario_datos["Ciudad/Estación"].append(datos[0])
+        diccionario_datos["Fecha"].append(datos[1])
+        diccionario_datos["Hora"].append(datos[2])
+        diccionario_datos["Condición del cielo"].append(datos[3])
+        diccionario_datos["Visibilidad"].append(datos[4])
+        diccionario_datos["Temperatura"].append(datos[5])
+        diccionario_datos["Sensación térmica"].append(datos[6])
+        diccionario_datos["Humedad"].append(datos[7])
+        diccionario_datos["Viento"].append(datos[8])
+        diccionario_datos["Presión"].append(datos[9])
+    print(diccionario_datos)
+        
+    
+
+        
+
+    
+
